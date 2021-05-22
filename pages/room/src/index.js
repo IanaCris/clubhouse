b7ba776dac852b90cd0291e5a33ed9195d4ae47f
IanaCris <ianasousati@gmail.com>
2021-05-22 19:07:47 -0300

    socket builder

---
+++ pages/room/src/index.js
@@ -0,0 +1,22 @@
+import { constants } from '../../_shared/constants.js';
+import SocketBuilder from '../../_shared/socketBuilder.js';
+
+const socketBuilder = new SocketBuilder({
+  socketUrl: constants.socketUrl,
+  namespace: constants.socketNamespaces.room
+})
+
+const socket = socketBuilder
+  .setOnUserConnected((user) => console.log('User connected!', user))
+  .setOnUserDisconnected((user) => console.log('User disconnected!', user))
+  .build();
+
+const room = {
+  id: Date.now(),
+  topic: 'JS Expert'
+}
+
+const user = {
+  img: 'https://cdn3.iconfinder.com/data/icons/generic-avatars/128/avatar_portrait_woman_female_1-128.png',
+  username: 'Cristina'
+}
